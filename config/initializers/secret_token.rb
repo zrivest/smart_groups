657a5548af958b3d15852ec3deceaaf699febedb
zrivest <zrivest@gmail.com>
2013-11-08 15:38:07 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartGroups::Application.config.secret_token = '5db80156b43ac83d34c5311863dbdfffcbc131f5ff5d81b27b41377dbc0f19f8d11dbfebb890601c62e620a46b9c5a6081e7e82deecd486a089fd48d52082f1d'
