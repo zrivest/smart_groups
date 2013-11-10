1c1f9cfd5aa6f0bd9baac357101543df2486c845
zrivest <zrivest@gmail.com>
2013-11-10 16:59:28 -0600

    added queries.txt file for association notes

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,7 +5,7 @@ class Group < ActiveRecord::Base
   has_many :enrollments, through: :pods
   has_many :students, through: :enrollments
   has_many :student_assignments, through: :students
-  has_many :assignments, through: :student_assignmentsr
+  has_many :assignments, through: :student_assignments
   belongs_to :course
 
 end
