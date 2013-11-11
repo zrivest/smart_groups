b9dc52806235987f249ef56e17604e5a22ea0194
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-10 19:07:15 -0600

    completed even distribution of grades algorithm

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -13,43 +13,41 @@ class CoursesController < ApplicationController
   end
 
   def num_groups
-    # binding.pry
     students = User.find(session[:user_id]).courses.find(params[:id]).students
     @students = students.uniq{ |student| student.id }
     num_students = @students.length
     num_groups = params[:course][:num_of_groups].to_i
+    
     if params[:course][:random].to_i == 1
       Course.random(@students)
       @groups = Course.total_num_groups(num_students, num_groups, @students)
     end
-    if params[:course][:even_grade_distribution].to_i == 1
-      @students.each do |student|
-        assignments = []
-        StudentAssignment.where(student_id: student.id).each do |assignment|
-          assignments << assignment.grade
-        end
-        sum = 0
-        assignments.each{|n| sum += n}
-        @average = sum/(assignments.length)
-        student.update_attributes!(average: @average)
-      end  
-      @students.sort_by!(&:average)
-      @groups = Course.total_num_even_groups(num_students, num_groups, @students)
 
+    if params[:course][:even_grade_distribution].to_i == 1
+      Course.average(@students)
+      @groups = Course.total_num_even_groups(num_students, num_groups, @students)
+    end
 
     render :groups
-    
-    end
   end
+
 
   def groups
     students = User.find(session[:user_id]).courses.find(params[:id]).students
     @students = students.uniq{ |student| student.id }
     num_students = @students.length
     students_per_group = params[:course][:num_per_group].to_i
-    Course.random(@students)
-    @groups = Course.total_students_groups(num_students, students_per_group, @students)
     
+    if params[:course][:random].to_i == 1
+      Course.random(@students)
+      @groups = Course.total_students_groups(num_students, students_per_group, @students)
+    end
+
+    if params[:course][:even_grade_distribution].to_i == 1
+      Course.average(@students)
+      @groups = Course.total_students_even_groups(num_students, students_per_group, @students)
+    end
+
     render :groups
   end
 end
