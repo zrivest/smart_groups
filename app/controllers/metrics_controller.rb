c8de3eee6a010db08f9c81cafbdf6c38b1944bc1
Ryan Kelley <rskelley9@gmail.com>
2013-11-12 01:51:21 -0600

    added new chart type in view(a second: spline chart shows student assignment vs student assignment scores;

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -14,7 +14,8 @@ class MetricsController < ApplicationController
     @categories = Array.new
     @student_names = Array.new
     @hash = {}
-        @completed_assignments.each do |student_assignment|
+    @completed_assignments.each do |student_assignment|
+      @categories << "#{student_assignment.assignment.assignment_name}"
           #assignment name
           @student_names << student_assignment.student.first_name
           #assignment grade
@@ -23,43 +24,44 @@ class MetricsController < ApplicationController
         end
 
 
-
-
-
-
-
-    p "---------------------------------------"
-    @hash.each do |k,v|
-      p "THIS IS THE KEY: #{k}"
-      p "THIS IS THE VAL: #{v}"
-    end
-    p "---------------------------------------"
+        p "---------------------------------------"
+        @hash.each do |k,v|
+          p "THIS IS THE KEY: #{k}"
+          p "THIS IS THE VAL: #{v}"
+        end
+        p @categories
+        p "---------------------------------------"
 
 
         # type: "column", name: student name, data: all their grades
        # (:type=> "column",:name=> student_assignment.assignment.assignment_name,:data=> grades)
 
-      
 
-      
-    @chart = LazyHighCharts::HighChart.new('graph') do |f|
-      f.title({ :text=>@course.name})
-      @hash.each do |k,v|
-        f.series(:type=> "column",:name=> "#{k}", :data=> v)
-      end  
-       
+       @chart = LazyHighCharts::HighChart.new('graph') do |f|
+        f.title({ :text=>@course.name})
+        @hash.each do |k,v|
+          f.series(:type=> "column",:name=> "#{k}", :data=> v)
+        end
 
-      f.options[:xAxis][:categories] = ["Math Test1", "History HW", "Python","BlackJack","Poker"]
-      f.labels(:items=>[:html=>"Total fruit consumption", :style=>{:left=>"40px", :top=>"8px", :color=>"black"} ])
+
+        f.options[:xAxis][:categories] = @categories
+        f.labels(:items=>[:html=>"Student Test Grades", :style=>{:left=>"40px", :top=>"8px", :color=>"black"} ])
+      end
+
+      @chart_area = LazyHighCharts::HighChart.new('graph') do |f|
+        f.options[:chart][:defaultSeriesType] = "spline"
+        @hash.each do |k,v|
+          f.series(:type=> "spline",:name=> "#{k}", :data=> v)
+        end
+        f.options[:chart][:inverted] = false
+        f.options[:legend][:layout] = "horizontal"
+        f.options[:xAxis][:categories] = @categories
+      end
+
+      render :show
     end
-    render :show
-  end
-end
 
-#   @course.all_completed_assignments_for_course
-#     @average_grades
-#     end
-#     render :show
-#   end
-# end
+
+  end
+
 
