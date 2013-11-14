2b0ac531ea1008b185c81b461dcefdfea872fefd
Ryan Kelley <rskelley9@gmail.com>
2013-11-14 02:42:07 -0600

    removed binding.pry from metrics controller

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -4,7 +4,6 @@ class MetricsController < ApplicationController
     @course = Course.find(params[:course_id])
     session[:course_id] = @course.id
     @chart_types = [{id: 1, type: "Each Student's Assignment Scores by Assignment"}, {id: 2, type: "Individual Student Performance by Assignment Due Date"}, {id: 3, type: "Class Average Assignment Scores by Due Date"}]
-    binding.pry
   end
 
   def create
